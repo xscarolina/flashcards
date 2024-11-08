e73cdddca3f029b9300b631b39e61b8bcf2a0bac
xscarolina <leonardocarolina24@gmail.com>
2024-11-08 08:07:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Palmeiras',
+    'Qual o melhor zagueiro?',
+    'Piquerez'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Em qual ano foi fundado o time?',
+    ' Fundado em 26 de agosto de 1914'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantos titulos o time possui?',
+    'Possui mais de 350 titulos apenas no futebol'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Como o Palestra Italia virou o Palmeiras?',
+    'O clube alviverde foi forçado a mudar de nome em 1942, devido altercações com o governo brasileiro, e passou a chamar-se Sociedade Esportiva Palmeiras.'
+)
